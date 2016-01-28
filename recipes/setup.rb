9ec67f6bbe2e32525427cd3887ef80286c0d708b
PK035158 <praveereddy.katta@cerner.com>
2016-01-28 22:16:41 +0000

    Added erbs

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -4,10 +4,6 @@ package 'emacs'
 package 'tree'
 package 'git'
 
-file '/etc/motd' do
-   content "Property of praveen!
-   IPADDRESS: #{node['ipaddress']}
-   HOSTNAME: #{node['hostname']}
-   MEMORY: #{node['memory']['total']}
-   CPU: #{node['cpu']['0']['mhz']}"
+template '/etc/motd' do
+   source 'motd.erb'
 end
